fb65b5aaec82af40de3691154970df21ce46b3b6
aguskrmn <acarman@udesa.edu.ar>
2023-11-19 19:28:56 -0300

    un dia haciendo profile

--- src/screens/User/User.js
+++ src/screens/User/User.js
@@ -1,29 +1,65 @@
 import react, { Component } from "react";
-import {
-  TextInput,
-  TouchableOpacity,
-  View,
-  Text,
-  StyleSheet,
-  FlatList 
-} from "react-native";
+import {db, auth } from '../../firebase/config';
+import {TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList, ScrollView, Image} from 'react-native';
+import {updatePassword} from 'firebase/auth'
 
-
-
-
-
-class User extends Component {
-  
-  render() {
-    
-    return (
-      <View> 
-         <Text>Hola user</Text>
- 
-      </View>
-    );
+class MyProfile extends Component{
+  constructor(props){
+      super(props)
+      this.state={
+          posteos: [],
+          dataUsuario: {},
+          id: '',
+          usuarioLogueado: auth.currentUser.email,
+      };
   }
-}
 
+  componentDidMount(){
+    db.collection('posts').where('owner', '==', this.state.usuarioLogueado)
+      .onSnapshot(listaPosts => {
+              let posts = [];
+              listaPosts.forEach(unPost => {
+                  posts.push({
+                      id: unPost.id,
+                      datos: unPost.data()
+                  })
+              })
 
-export default User;
+              this.setState({
+                  posteos: posts
+              })
+              console.log("posteos", this.state.posteos);
+          }
+      )
+
+      db.collection('users')
+          .where('owner', '==', this.state.usuarioLogueado)
+          .onSnapshot(docs => {
+              docs.forEach(doc =>
+                  this.setState({
+                      id: doc.id,
+                      dataUsuario: doc.data()
+                  }))
+          })
+  }
+  
+ 
+  logout() {
+  auth.signOut();
+  this.props.navigation.navigate("Login");
+  }
+
+  render() {
+    console.log('perfil')
+      return (
+        <View>
+        <Text>Bienvenido {this.state.dataUsuario.owner}</Text>
+        <Text>Biografia: {this.state.dataUsuario.description}</Text>
+        <Text>Mail: {auth.currentUser.email}</Text>
+    </View>
+      )
+    
+    }
+  }
+
+export default MyProfile;
